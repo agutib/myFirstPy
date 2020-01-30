5081eb2a9d8755fce2e25762a905cd4ba00fb2df
isiKumonryu <agutib@innovuze.com>
2020-01-30 13:26:47 +0800

    Reverted

--- djangooo/blog/models.py
+++ djangooo/blog/models.py
@@ -10,7 +10,6 @@ class Post (models.class (models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
-    
  def publish(self):
         self.published_date = timezone.now()
         self.save()
