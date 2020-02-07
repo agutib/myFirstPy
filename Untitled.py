977c05cd4ac717ef84146a199b00e39b943128b5
isiKumonryu <agutib@innovuze.com>
2020-02-07 09:28:17 +0800

    Added lines of Untitled.py from line 7 downwards

--- Untitled.py
+++ Untitled.py
@@ -3,3 +3,7 @@
 birthYear = input ("Birth Year : ")
 age = 2019 - int(birthYear)
 print("Your Age " + str(age))
+
+mass = input ("Your Weight in lb : ")
+convert = 0.453592 * float(mass)
+print ("Your weight in Kg is : " + str(convert))
